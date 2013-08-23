3caff3b0aabe3a96db5859d6adb614bad4921bbf
Chris Leppanen <chris.leppanen@gmail.com>
2013-08-22 20:13:39 -0700

    Add InterfaceNotImplementedException.

---
+++ lib/Drupal/feeds/Exception/InterfaceNotImplementedException.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\feeds\Exception\InterfaceNotImplementedException.
+ */
+
+ namespace Drupal\feeds\Exception;
+
+ use BadMethodCallException;
+
+class InterfaceNotImplementedException extends BadMethodCallException {}
