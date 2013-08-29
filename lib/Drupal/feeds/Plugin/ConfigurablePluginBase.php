719fda3501e9567a4cd6a7b32d8d0c6dad3c90e5
Chris Leppanen <chris.leppanen@gmail.com>
2013-08-28 19:09:38 -0700

    Move translation helpers to PluginBase.

--- lib/Drupal/feeds/Plugin/ConfigurablePluginBase.php
+++ lib/Drupal/feeds/Plugin/ConfigurablePluginBase.php
@@ -18,13 +18,6 @@ use Drupal\feeds\Plugin\PluginBase;
 abstract class ConfigurablePluginBase extends PluginBase implements ConfigurablePluginInterface, PluginFormInterface {
 
   /**
-   * The translation manager service.
-   *
-   * @var \Drupal\Core\StringTranslation\TranslationInterface
-   */
-  protected $translationManager;
-
-  /**
    * Constructs a ConfigurablePluginBase object.
    *
    * @param array $configuration
@@ -92,55 +85,6 @@ abstract class ConfigurablePluginBase extends PluginBase implements Configurable
   public function submitConfigurationForm(array &$form, array &$form_state) {
     $configuration = $form_state['values'][$this->pluginType()]['configuration'];
     $this->setConfiguration($configuration);
-  }
-
-  /**
-   * Translates a string to the current language or to a given language.
-   *
-   * @param string $string
-   *   A string containing the English string to translate.
-   * @param array $args
-   *   An associative array of replacements to make after translation. Based
-   *   on the first character of the key, the value is escaped and/or themed.
-   *   See \Drupal\Core\Utility\String::format() for details.
-   * @param array $options
-   *   An associative array of additional options, with the following elements:
-   *   - 'langcode': The language code to translate to a language other than
-   *      what is used to display the page.
-   *   - 'context': The context the source string belongs to.
-   *
-   * @return string
-   *   The translated string.
-   */
-  protected function t($string, array $args = array(), array $options = array()) {
-    return $this->getTranslationManager()->translate($string, $args, $options);
-  }
-
-  /**
-   * Gets the translation manager.
-   *
-   * @return \Drupal\Core\StringTranslation\TranslationInterface
-   *   The translation manager.
-   */
-  protected function getTranslationManager() {
-    if (!$this->translationManager) {
-      $this->translationManager = \Drupal::translation();
-    }
-    return $this->translationManager;
-  }
-
-  /**
-   * Sets the translation manager for this form.
-   *
-   * @param \Drupal\Core\StringTranslation\TranslationInterface $translation_manager
-   *   The translation manager.
-   *
-   * @return self
-   *   The entity form.
-   */
-  public function setTranslationManager(TranslationInterface $translation_manager) {
-    $this->translationManager = $translation_manager;
-    return $this;
   }
 
 }
