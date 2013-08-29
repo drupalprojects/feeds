f2bff9faef085a4b4e81639d8fe176ae016be339
Chris Leppanen <chris.leppanen@gmail.com>
2013-08-28 17:58:11 -0700

    Convert FeedController to ControllerBase.

--- lib/Drupal/feeds/Controller/FeedController.php
+++ lib/Drupal/feeds/Controller/FeedController.php
@@ -7,68 +7,15 @@
 
 namespace Drupal\feeds\Controller;
 
-use Drupal\Component\Plugin\PluginManagerInterface;
-use Drupal\Core\Controller\ControllerInterface;
-use Drupal\Core\Entity\EntityStorageControllerInterface;
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\feeds\FeedInterface;
 use Drupal\feeds\Entity\Feed;
 use Drupal\feeds\Entity\Importer;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Returns responses for feed routes.
  */
-class FeedController implements ControllerInterface {
-
-  /**
-   * The entity manager.
-   *
-   * @var \Drupal\Component\Plugin\PluginManagerInterface
-   */
-  protected $entityManager;
-
-  /**
-   * The feed storage controller.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageControllerInterface
-   */
-  protected $feedStorage;
-
-  /**
-   * The importer storage controller.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageControllerInterface
-   */
-  protected $importerStorage;
-
-  /**
-   * Constructs a FeedController object.
-   *
-   * @param \Drupal\Component\Plugin\PluginManagerInterface $entity_manager
-   *   The Entity manager.
-   * @param \Drupal\Core\Entity\EntityStorageControllerInterface $feed_storage
-   *   The feed storage controller.
-   * @param \Drupal\Core\Entity\EntityStorageControllerInterface $importer_storage
-   *   The feed importer controller.
-   */
-  public function __construct(PluginManagerInterface $entity_manager, EntityStorageControllerInterface $feed_storage, EntityStorageControllerInterface $importer_storage) {
-    $this->entityManager = $entity_manager;
-    $this->feedStorage = $feed_storage;
-    $this->importerStorage = $importer_storage;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    $entity_manager = $container->get('plugin.manager.entity');
-    return new static(
-      $entity_manager,
-      $entity_manager->getStorageController('feeds_feed'),
-      $entity_manager->getStorageController('feeds_importer')
-    );
-  }
+class FeedController extends ControllerBase {
 
   /**
    * Presents the feeds feed creation form.
@@ -78,32 +25,31 @@ class FeedController implements ControllerInterface {
    *
    * @todo Return a render array/twig template?
    */
-  public function add(Request $request) {
-
-    $account = $request->attributes->get('_account');
-
+  public function add() {
     // Show add form if there is only one importer.
-    $importers = $this->importerStorage->loadEnabled();
+    $importers = $this->entityManager()
+      ->getStorageController('feeds_importer')
+      ->loadEnabled();
     // There is an access checker on this route that determines if the user can
     // create at least one importer. If there is only one enabled importer, this
     // must be it.
     if ($importers && count($importers) == 1) {
       $importer = reset($importers);
-      return $this->addForm($importer, $request);
+      return $this->addForm($importer);
     }
 
     // @todo Don't show link for non-admins.
-    $empty = t('There are no importers, go to <a href="@importers">Feed importers</a> to create one or enable an existing one.', array('@importers' => url('admin/structure/feeds')));
+    $empty = $this->t('There are no importers, go to <a href="@importers">Feed importers</a> to create one or enable an existing one.', array('@importers' => url('admin/structure/feeds')));
 
     $build = array(
       '#theme' => 'table',
-      '#header' => array(t('Import'), t('Description')),
+      '#header' => array($this->t('Import'), $this->t('Description')),
       '#rows' => array(),
       '#empty' => $empty,
     );
 
     foreach ($importers as $importer) {
-      if (!($importer->access('create', $account))) {
+      if (!($importer->access('create'))) {
         continue;
       }
       $link = 'feed/add/' . $importer->id();
@@ -120,17 +66,17 @@ class FeedController implements ControllerInterface {
    * @return array
    *   A form array as expected by drupal_render().
    */
-  public function addForm(Importer $feeds_importer, Request $request) {
-    $account = $request->attributes->get('_account');
+  public function addForm(Importer $feeds_importer) {
+    $account = $this->currentUser();
 
-    $feed = $this->feedStorage->create(array(
+    $feed = $this->entityManager()->getStorageController('feeds_feed')->create(array(
       'uid' => $account->id(),
       'importer' => $feeds_importer->id(),
       'status' => 1,
       'created' => REQUEST_TIME,
     ));
 
-    return $this->entityManager->getForm($feed, 'create');
+    return $this->entityManager()->getForm($feed, 'create');
   }
 
   /**
@@ -139,8 +85,8 @@ class FeedController implements ControllerInterface {
    * @return array
    *   A form array as expected by drupal_render().
    */
-  public function view(Feed $feeds_feed, Request $request) {
-    return $this->entityManager
+  public function view(Feed $feeds_feed) {
+    return $this->entityManager()
       ->getRenderController('feeds_feed')
       ->view($feeds_feed);
   }
