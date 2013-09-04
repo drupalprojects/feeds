6731ad81241341017c4fd7f676aa6a4a9b7e238f
Chris Leppanen <chris.leppanen@gmail.com>
2013-09-04 04:49:04 -0700

    Add email target.

---
+++ lib/Drupal/feeds/Feeds/Target/Email.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\feeds\Feeds\Target\Email.
+ */
+
+namespace Drupal\feeds\Feeds\Target;
+
+use Drupal\Component\Annotation\Plugin;
+use Drupal\feeds\Plugin\Type\Target\FieldTargetBase;
+
+/**
+ * Defines a email field mapper.
+ *
+ * @Plugin(
+ *   id = "email",
+ *   field_types = {"email_field"}
+ * )
+ */
+class Email extends FieldTargetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function prepareValue($delta, array &$values) {
+    $values['value'] = trim($values['value']);
+    if (!filter_var($values['value'], FILTER_VALIDATE_EMAIL)){
+      $values['value'] = '';
+    }
+  }
+
+}
