c09cae0a6debe590c2a2f41f28c3b7b315dc3647
Chris Leppanen <chris.leppanen@gmail.com>
2013-09-24 23:07:34 -0700

    Fix DateTime target.

--- lib/Drupal/feeds/Feeds/Target/DateTime.php
+++ lib/Drupal/feeds/Feeds/Target/DateTime.php
@@ -32,7 +32,7 @@ class DateTime extends FieldTargetBase {
    */
   public function __construct(array $settings, $plugin_id, array $plugin_definition) {
     parent::__construct($settings, $plugin_id, $plugin_definition);
-    $datetime_type = $this->settings['instance']->getFieldSetting('datetime_type');
+    $datetime_type = $this->settings['settings']['datetime_type'];
     $this->storageFormat = $datetime_type == 'date' ? DATETIME_DATE_STORAGE_FORMAT : DATETIME_DATETIME_STORAGE_FORMAT;
   }
 
