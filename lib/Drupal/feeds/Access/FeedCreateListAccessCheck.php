2d9fe0dd1ab5bf32e32fdbcf9965e79b05840d69
Chris Leppanen <chris.leppanen@gmail.com>
2013-10-15 23:38:10 -0700

    Update routing and access checking.

--- lib/Drupal/feeds/Access/FeedCreateListAccessCheck.php
+++ lib/Drupal/feeds/Access/FeedCreateListAccessCheck.php
@@ -7,58 +7,33 @@
 
 namespace Drupal\feeds\Access;
 
-use Drupal\Core\Access\StaticAccessCheckInterface;
-use Drupal\Core\Entity\EntityManager;
+use Drupal\Core\Entity\EntityCreateAccessCheck;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Route;
 
 /**
  * Access check for feeds link add list routes.
  */
-class FeedCreateListAccessCheck implements StaticAccessCheckInterface {
-
-  /**
-   * The importer storage controller.
-   *
-   * @var \Drupal\Core\Entity\EntityStorageControllerInterface
-   */
-  protected $importerStorage;
-
-  /**
-   * The feed access controller.
-   *
-   * @var \Drupal\Core\Entity\EntityAccessControllerInterface
-   */
-  protected $feedAccessController;
-
-  /**
-   * Constructs a FeedCreateListAccessCheck object.
-   *
-   * @param \Drupal\Core\Entity\EntityManager $entity_manager
-   *   The Entity manager.
-   */
-  public function __construct(EntityManager $entity_manager) {
-    $this->importerStorage = $entity_manager->getStorageController('feeds_importer');
-    $this->feedAccessController = $entity_manager->getAccessController('feeds_feed');
-  }
+class FeedCreateListAccessCheck extends EntityCreateAccessCheck {
 
   /**
    * {@inheritdoc}
    */
-  public function appliesTo() {
-    return array('_access_feeds_feed_create_list');
-  }
+  protected $requirementsKey = '_feeds_feed_add_access';
 
   /**
    * {@inheritdoc}
    */
   public function access(Route $route, Request $request) {
     // @todo Perhaps read config directly rather than load all importers.
-    foreach ($this->importerStorage->loadEnabled() as $importer) {
-      if ($this->feedAccessController->createAccess($importer->id())) {
+    $access_controller = $this->entityManager->getAccessController('feeds_feed');
+    foreach ($this->entityManager->getStorageController('feeds_importer')->loadEnabled() as $importer) {
+      if ($access_controller->createAccess($importer->id())) {
         return self::ALLOW;
       }
     }
+
+    return static::DENY;
   }
 
 }
