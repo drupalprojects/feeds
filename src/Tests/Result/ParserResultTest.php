9b44547fb7d5a681ab940b30707a508b8fc09ff1
Chris Leppanen <chris.leppanen@gmail.com>
2014-09-22 14:47:18 -0700

    Add ParserResultTest

---
+++ src/Tests/Result/ParserResultTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\feeds\Tests\Result\ParserResultTest.
+ */
+
+namespace Drupal\feeds\Tests\Result;
+
+use Drupal\feeds\Result\ParserResult;
+use Drupal\feeds\Tests\FeedsUnitTestCase;
+
+/**
+ * @coversDefaultClass \Drupal\feeds\Result\ParserResult
+ * @group Feeds
+ */
+class ParserResultTest extends FeedsUnitTestCase {
+
+  public function test() {
+    $result = new ParserResult();
+    $this->assertSame($result, $result->set('attr', 'value1'));
+    $this->assertSame($result->get('attr'), 'value1');
+
+    // Test item adding.
+    $item = $this->getMock('Drupal\feeds\Feeds\Item\ItemInterface');
+    $this->assertSame($result, $result->addItem($item));
+
+    // Check countable.
+    $this->assertSame(1, count($result));
+  }
+
+}
