119ac04461898e9e88c7c314d489c83e72ec3c32
Chris Leppanen <chris.leppanen@gmail.com>
2014-09-22 14:34:03 -0700

    Refactor FetcherResult.

--- src/Result/FetcherResult.php
+++ src/Result/FetcherResult.php
@@ -35,21 +35,32 @@ class FetcherResult implements FetcherResultInterface {
    * {@inheritdoc}
    */
   public function getRaw() {
-    $raw = file_get_contents($this->filePath);
-    if ($raw === FALSE) {
-      $this->error('The file %file is not readable.');
-    }
-    return $this->sanitizeRaw($raw);
+    $this->checkFile();
+    return $this->sanitizeRaw(file_get_contents($this->filePath));
   }
 
   /**
    * {@inheritdoc}
    */
   public function getFilePath() {
-    if (!file_exists($this->filePath)) {
-      $this->error('File %filepath is not accessible.');
-    }
+    $this->checkFile();
     return $this->sanitizeFile();
+  }
+
+  /**
+   * Checks that a file exists and is readable.
+   *
+   * @throws \RuntimeException
+   *   Thrown if the file isn't readable or writable.
+   */
+  protected function checkFile() {
+    if (!file_exists($this->filePath)) {
+      throw new \RuntimeException(String::format('File %filepath does not exist.', array('%filepath' => $this->filePath)));
+    }
+
+    if (!is_readable($this->filePath)) {
+      throw new \RuntimeException(String::format('File %filepath is not readable.', array('%filepath' => $this->filePath)));
+    }
   }
 
   /**
@@ -86,40 +97,24 @@ class FetcherResult implements FetcherResultInterface {
    */
   protected function sanitizeFile() {
     $handle = fopen($this->filePath, 'r');
-
-    // This should rarely happen since we already checked if the file exists.
-    if ($handle === FALSE) {
-      $this->error('File %filepath is not readable.');
-    }
     $line = fgets($handle);
     fclose($handle);
 
     // If BOM header is present, read entire contents of file and overwrite the
     // file with corrected contents.
-    if (substr($line, 0, 3) == pack('CCC', 0xef, 0xbb, 0xbf)) {
-      $contents = file_get_contents($this->filePath);
-      $contents = substr($contents, 3);
-      $status = file_put_contents($this->filePath, $contents);
-      if ($status === FALSE) {
-        $this->error('File %filepath is not writeable.');
-      }
+    if (substr($line, 0, 3) !== pack('CCC', 0xef, 0xbb, 0xbf)) {
+      return $this->filePath;
     }
 
-    return $this->filePath;
-  }
+    if (!is_writable($this->filePath)) {
+      throw new \RuntimeException(String::format('File %filepath is not writable.', array('%filepath' => $this->filePath)));
+    }
 
-  /**
-   * Throws an exception on file error.
-   *
-   * @param string $message
-   *   The message to display. The filepath will be substituted in for
-   *   %filepath.
-   *
-   * @throws \RuntimeException
-   *   This always throws an exception, that's its job.
-   */
-  protected function error($message) {
-    throw new \RuntimeException(String::format($message, array('%filepath' => $this->filePath)));
+    $contents = file_get_contents($this->filePath);
+    $contents = substr($contents, 3);
+    $status = file_put_contents($this->filePath, $contents);
+
+    return $this->filePath;
   }
 
 }
--- src/Tests/Result/FetcherResultTest.php
+++ src/Tests/Result/FetcherResultTest.php
@@ -5,7 +5,7 @@
  * Contains \Drupal\feeds\Tests\Result\FetcherResultTest.
  */
 
-namespace Drupal\feeds\Tests;
+namespace Drupal\feeds\Tests\Result;
 
 use Drupal\feeds\Result\FetcherResult;
 use Drupal\feeds\Tests\FeedsUnitTestCase;
@@ -19,49 +19,55 @@ class FetcherResultTest extends FeedsUnitTestCase {
 
   public static function getInfo() {
     return array(
-      'name' => 'Feeds: Fetcher result',
+      'name' => 'Result: fetcher',
       'description' => 'Tests fetcher result.',
       'group' => 'Feeds',
     );
   }
 
-  /**
-   * @expectedException \RuntimeException
-   */
   public function testGetRaw() {
     file_put_contents(static::FILE, pack('CCC', 0xef, 0xbb, 0xbf) . 'I am test data.');
     $result = new FetcherResult(static::FILE);
     $this->assertSame('I am test data.', $result->getRaw());
+  }
 
-    // Throws exception.
-    $result = new FetcherResult('IDONTEXIST');
+  public function testGetFilePath() {
+    file_put_contents(static::FILE, 'I am test data.');
+    $result = new FetcherResult(static::FILE);
+    $this->assertSame(static::FILE, $result->getFilePath());
+  }
+
+  public function testGetSanitizedFilePath() {
+    file_put_contents(static::FILE, pack('CCC', 0xef, 0xbb, 0xbf) . 'I am test data.');
+    $result = new FetcherResult(static::FILE);
+    $this->assertSame('I am test data.', file_get_contents($result->getFilePath()));
+  }
+
+  /**
+   * @expectedException \RuntimeException
+   */
+  public function testNonExistantFile() {
+    $result = new FetcherResult('IDONOTEXIST');
     $result->getRaw();
   }
 
   /**
    * @expectedException \RuntimeException
    */
-  public function testGetFilePath() {
-    file_put_contents(static::FILE, pack('CCC', 0xef, 0xbb, 0xbf) . 'I am test data.');
+  public function testNonReadableFile() {
+    file_put_contents(static::FILE, 'I am test data.');
+    chmod(static::FILE, 000);
     $result = new FetcherResult(static::FILE);
-    $this->assertSame(static::FILE, $result->getFilePath());
-
-    file_put_contents(static::FILE, pack('CCC', 0xef, 0xbb, 0xbf) . 'I am test data.');
-    $result = new FetcherResult(static::FILE);
-    chmod(static::FILE, 0444);
-    $this->assertSame(static::FILE, $result->getFilePath());
-
-    // Throws exception.
-    $result = new FetcherResult('IDONTEXIST');
-    $result->getFilePath();
+    $result->getRaw();
   }
 
   /**
    * @expectedException \RuntimeException
    */
-  public function testGetFilePathNonExistentFile() {
-    // Throws exception.
-    $result = new FetcherResult('IDONTEXIST');
+  public function testNonWritableFile() {
+    file_put_contents(static::FILE, pack('CCC', 0xef, 0xbb, 0xbf) . 'I am test data.');
+    chmod(static::FILE, 0444);
+    $result = new FetcherResult(static::FILE);
     $result->getFilePath();
   }
 
