21f9b033ef27ad79699cb4e1b14a54b3144790df
rteijeiro <rteijeiro@780508.no-reply.drupal.org>
2015-05-20 05:33:02 -0700

    Issue #2490164 by rteijeiro: Feeds ESLint errors

--- feeds.js
+++ feeds.js
@@ -1,3 +1,6 @@
-Drupal.AjaxCommands.prototype.feedsHash = function(ajax, response, status) {
+Drupal.AjaxCommands.prototype.feedsHash = function (ajax, response, status) {
+
+  "use strict";
+
   window.location.hash = response.hash;
-}
+};
