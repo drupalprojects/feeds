0e6ec06fc6c1576995bd531f899e30dde0105538
Chris Leppanen <chris.leppanen@gmail.com>
2015-06-04 03:51:24 -0700

    Add required FeedViewsData class.

---
+++ src/FeedViewsData.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\feeds\FeedViewsData.
+ */
+
+namespace Drupal\feeds;
+
+use Drupal\views\EntityViewsData;
+
+/**
+ * Provides the views data for the feeds_feed entity type.
+ */
+class FeedViewsData extends EntityViewsData {
+
+}
