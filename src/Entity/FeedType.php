3536124e7c8737d1b1e541108daa147096efff64
megachriz <megachriz@654114.no-reply.drupal.org>
2017-07-02 10:14:47 +0200

    Issue #2869350 by MegaChriz, Erik Frèrejean: Provide one queue for all import related tasks.
    The queues 'feeds_feed_import', 'feeds_feed_parse' and 'feeds_feed_process' are removed. These queues are replaced by one queue called 'feeds_feed_refresh'.

--- src/Entity/FeedType.php
+++ src/Entity/FeedType.php
@@ -468,7 +468,7 @@ class FeedType extends ConfigEntityBundleBase implements FeedTypeInterface, Enti
       // Clear the queue worker plugin cache so that our derivatives will be
       // found.
       \Drupal::service('plugin.manager.queue_worker')->clearCachedDefinitions();
-      \Drupal::queue('feeds_feed_import:' . $this->id())->createQueue();
+      \Drupal::queue('feeds_feed_refresh:' . $this->id())->createQueue();
     }
   }
 
@@ -476,18 +476,14 @@ class FeedType extends ConfigEntityBundleBase implements FeedTypeInterface, Enti
    * {@inheritdoc}
    */
   public static function postDelete(EntityStorageInterface $storage, array $entities) {
-    $prefixes = ['feeds_feed_import', 'feeds_feed_parse', 'feeds_feed_process'];
-
     foreach ($entities as $entity) {
       foreach ($entity->getPlugins() as $plugin) {
         $plugin->onFeedTypeDelete();
       }
 
       // Delete any existing queues related to this type.
-      foreach ($prefixes as $prefix) {
-        if ($queue = \Drupal::queue($prefix . ':' . $entity->id())) {
-          $queue->deleteQueue();
-        }
+      if ($queue = \Drupal::queue('feeds_feed_refresh:' . $entity->id())) {
+        $queue->deleteQueue();
       }
     }
 
