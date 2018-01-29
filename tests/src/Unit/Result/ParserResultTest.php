4437b9b515f8981481e6232ef638f9d1fdcca1cf
megachriz <megachriz@654114.no-reply.drupal.org>
2018-01-29 17:00:57 +0100

    Issue #2940356 by MegaChriz: Added test coverage for the ParserResult class.

--- tests/src/Unit/Result/ParserResultTest.php
+++ tests/src/Unit/Result/ParserResultTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\feeds\Unit\Result;
 
+use Drupal\feeds\Feeds\Item\ItemInterface;
 use Drupal\feeds\Result\ParserResult;
 use Drupal\Tests\feeds\Unit\FeedsUnitTestCase;
 
@@ -12,10 +13,47 @@ use Drupal\Tests\feeds\Unit\FeedsUnitTestCase;
 class ParserResultTest extends FeedsUnitTestCase {
 
   /**
-   *
+   * @covers ::addItem
    */
-  public function test() {
+  public function testAddItem() {
+    $result = new ParserResult();
 
+    // Create some items.
+    $item1 = $this->getMock(ItemInterface::class);
+    $item2 = $this->getMock(ItemInterface::class);
+    $item3 = $this->getMock(ItemInterface::class);
+
+    // Add an item.
+    $result->addItem($item1);
+    $this->assertSame(1, $result->count());
+
+    // Add another two items.
+    $result->addItem($item2);
+    $result->addItem($item3);
+    $this->assertSame(3, $result->count());
+
+    $this->assertSame($item1, $result->offsetGet(0));
+    $this->assertSame($item2, $result->offsetGet(1));
+    $this->assertSame($item3, $result->offsetGet(2));
+  }
+
+  /**
+   * @covers ::addItems
+   */
+  public function testAddItems() {
+    $result = new ParserResult();
+
+    // Create some items.
+    $item1 = $this->getMock(ItemInterface::class);
+    $item2 = $this->getMock(ItemInterface::class);
+    $item3 = $this->getMock(ItemInterface::class);
+
+    $result->addItems([$item1, $item2, $item3]);
+    $this->assertSame(3, $result->count());
+
+    $this->assertSame($item1, $result->offsetGet(0));
+    $this->assertSame($item2, $result->offsetGet(1));
+    $this->assertSame($item3, $result->offsetGet(2));
   }
 
 }
